b14382641674487f98ba6d54d5e0ac39e019d685
Dorothy Bishop <dorothy.bishop@psy.ox.ac.uk>
2017-11-05 16:03:05 +0000

    Added lmer test of trisomy effect on subscales

--- dawba_select_data.R
+++ dawba_select_data.R
@@ -51,47 +51,33 @@ mytab<-table(my.dawba$tricode,my.dawba$allsubgp)
 tab.lobias<-mytab[,1:3]
 tab.socanx<-rbind(colSums(tab.lobias[1:3,]),colSums(tab.lobias[4:5,]))
 
-#analysis of SRS items - this is my own exploring - not v conclusive, ignore
-#add SRS items to my.dawba
-# w<-which(colnames(my.dawba1)=='srs_item1') #find col number for first SRS item
-# w2<-w+64
-# srsitems<-my.dawba1[,w:w2]
-# my.dawba<-cbind(my.dawba,srsitems)
-# 
-# #Just exploring data to see if any oddities in relation to +ve vs -ve items
-# anxpos<-c(3,11,43) #This is my personal classification of items
-# anxneg<-c(6,9,23,27,64)
-# asdpos<-c(12,15,17,21,22,26,40,45,48,52,55)
-# asdneg<-c(2,4,5,7,8,10,13,16,18,19,20,24,25,28,29:31,33,34:39,42,46,47,50,51,53,54,56,58,60:63,65)
-# other<-c(1,14,32,41,44,49,57,59)
-# 
-# w<-which(colnames(my.dawba)=='srs_item1') #find col number for first SRS item
-# w=w-1
-# 
-# anxcol1<-anxpos+w
-# anxcol2<-anxneg+w
-# asdcol1<-asdpos+w
-# asdcol2<-asdneg+w
-# othcol<-other+w
-# 
-# my.dawba$anxposmean<-rowMeans(my.dawba[,anxcol1],na.rm=TRUE)
-# my.dawba$anxnegmean<-rowMeans(my.dawba[,anxcol2],na.rm=TRUE)
-# my.dawba$asdposmean<-rowMeans(my.dawba[,asdcol1],na.rm=TRUE)
-# my.dawba$asdnegmean<-rowMeans(my.dawba[,asdcol2],na.rm=TRUE)
-# my.dawba$othmean<-rowMeans(my.dawba[,othcol],na.rm=TRUE)
-# 
-# 
-# #Now do the  plots on one pdf
-# mysrscats<-c('Anxpos','Anxneg','ASDpos','ASDneg','Other')
-# mycols<- c("grey66", "red", "hotpink1","blue","purple")
-# dir<
-#  pdfname<-paste0(dir,'beeswarm_dbscales.pdf')
-#  pdf(pdfname,width=8,height=5)
-# par(mfrow=c(2,3))
-# for (i in 1:5){
-#   boxplot(my.dawba[,(89+i)]~allsubgp , data = my.dawba)
-#   beeswarm(my.dawba[,(89+i)]~allsubgp , data = my.dawba, xlab='Trisomy',ylab=mysrscats[i],
-#                horizontal=FALSE,col = 5,cex.axis=.65, pch = 16,
-#            pwcol = mycols[as.numeric(tricode)],add=TRUE)
-# }
-# dev.off()
+#---------------------------------------------------------------------
+# Create long form file for SRS subscales, Low Bias only
+#---------------------------------------------------------------------
+lowbias<-filter(my.dawba,allsubgp=='XXX'|allsubgp=='XXY'|allsubgp=='XYY')
+table(lowbias$trisomy)
+lowbias.long<-dplyr::select(lowbias,record_id,trisomy)
+lowbias.long$subscale<-NA
+lowbias.long$score<-NA
+myrow<-nrow(lowbias)
+thisrow=0
+for (i in 6:10){
+ lowbias.long[(thisrow+1):(thisrow+myrow),3:4]<-cbind(i-5,lowbias[,i])
+ lowbias.long[(thisrow+1):(thisrow+myrow),1:2]<-lowbias.long[1:51,1:2]
+ thisrow<-thisrow+myrow
+}
+lowbias.long$trisomy<-as.factor(lowbias.long$trisomy)
+levels(lowbias.long$trisomy)<-c('XXX','XXY','XYY')
+
+w<-which(lowbias.long$score>90) #remove jitter that was added for beeswarm
+lowbias.long$score[w]<-90
+
+library(lme4)
+
+fit1a <- lmer(score ~ trisomy + subscale + trisomy*subscale + (1 | record_id),  
+              data = lowbias.long)
+#model 1 output
+summary(fit1a)
+
+
+
